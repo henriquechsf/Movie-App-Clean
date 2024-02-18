3de9ca478ef2984bac921d91e2a158fbb3d6cdc2
Carlos Henrique de S. Ferreira <carloshenrique4m@gmail.com>
2024-02-18 01:25:39 -0300

    17. Implementa NavigationGraph

--- app/src/main/java/br/com/movieapp/core/presentation/navigation/BottomNavItem.kt
+++ app/src/main/java/br/com/movieapp/core/presentation/navigation/BottomNavItem.kt
@@ -1,4 +1,4 @@
-package br.com.movieapp.presentation.navigation
+package br.com.movieapp.core.presentation.navigation
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
