bec9ca6c0671f629c20b6b8b925a94b539b763a7
Carlos Henrique de S. Ferreira <carloshenrique4m@gmail.com>
2024-02-25 23:00:17 -0300

    64 - Implementa MovieAppBar

--- app/src/main/java/br/com/movieapp/movie_detail_feature/presentation/MovieDetailScreen.kt
+++ app/src/main/java/br/com/movieapp/movie_detail_feature/presentation/MovieDetailScreen.kt
@@ -2,17 +2,13 @@ package br.com.movieapp.movie_detail_feature.presentation
 
 import android.annotation.SuppressLint
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.stringResource
 import androidx.paging.compose.collectAsLazyPagingItems
 import br.com.movieapp.R
 import br.com.movieapp.core.domain.model.Movie
+import br.com.movieapp.core.presentation.components.common.MovieAppBar
 import br.com.movieapp.movie_detail_feature.presentation.state.MovieDetailState
 import br.com.movieapp.movie_popular_feature.presentation.components.MovieDetailContent
-import br.com.movieapp.ui.theme.black
-import br.com.movieapp.ui.theme.white
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
@@ -24,12 +20,7 @@ fun MovieDetailScreen(
 
     Scaffold(
         topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = stringResource(id = R.string.detail_movie), color = white)
-                },
-                backgroundColor = black
-            )
+            MovieAppBar(title = R.string.detail_movie)
         },
         content = {
             MovieDetailContent(
